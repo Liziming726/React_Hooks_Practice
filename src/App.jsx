16e519cc267acf633a370578b9d7b4efd39326f2
zimingli <873884635@qq.com>
2022-09-10 10:03:33 +0800

    useEffect practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,29 @@
-import React,{useState,useEffect} from "react"
-// import { FaAngleDoubleRight } from 'react-icons/fa'
-
-const url = 'https://course-api.com/react-tabs-project'
+import React,{useEffect,useState} from "react"
+import axios from "axios"
 
 function App() {
-  const [loading,setLoading] = useState(true)
-  const [jobs,setJobs] = useState([])
-  const [value,setValue] = useState(0)
 
-  const fetchJobs = async () => {
-    const response = await fetch(url)
-    const newJobs = await response.json()
-    setJobs(newJobs)
-    setLoading(false)
-  }
+  const [data,setData] = useState('')
+  const [count,setCount] = useState(0)
 
   useEffect(() => {
-    fetchJobs()
+    axios.get("https://jsonplaceholder.typicode.com/comments")
+    .then((response)=>{
+      setData(response.data[0].email)
+      console.log("API was called");
+    })
   },[])
-  if(loading){
-    <section className="section loading">
-    <h1>Loading...</h1>
-  </section>
-  }
-
-  const {company,dates,duties,title} = jobs[value]
+  
   return (
     <>
-    <section className="section">
-    <div className="title">
-      <h2>Experience</h2>
-      <div className="underline"></div>
+    <div>
+      Hello axios
+      <h1>{data}</h1>
+      <h1>{count}</h1>
+      <button onClick={()=>{
+        setCount(count+1)
+      }}>Click</button>
     </div>
-    <div className="jobs-center">
-      <div className="btn-container">
-        {jobs.map((item,index) => {
-          return(<button
-            key={item.id}
-            onClick={() => setValue(index)}
-            className={`job-btn ${index === value && 'active-btn'}`}
-          >
-            {item.company}
-          </button>
-          )
-        })}
-        </div>
-        <article className="job-info">
-          <h3>{title}</h3>
-          <h4>{company}</h4>
-          <p className="job-date">{dates}</p>
-          {duties.map((duty, index) => {
-            return (
-              <div key={index} className="job-desc">
-                <FaAngleDoubleRight className="job-icon"></FaAngleDoubleRight>
-                <p>{duty}</p>
-              </div>
-            )
-          })}
-        </article>
-      </div>
-      <button type="button" className="btn">
-        more info
-      </button>
-    </section>
     </>
   )
 }
